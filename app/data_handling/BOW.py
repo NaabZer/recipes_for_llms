8de57776a46e385072a38a5730b2d29f06436c34
NaabZer <anton.gefvert@gmail.com>
2025-06-09 19:41:53 +0200

    Refactor notebook functions into files

---
+++ app/data_handling/BOW.py
@@ -0,0 +1,9 @@
+import polars as pl
+
+
+def transform_ds_to_BOW(ds: pl.DataFrame, model):
+    tfidfs = []
+    for ingredients in ds.select('ingredients').iter_rows():
+        datapoint_tfidf = model.transform(["\n".join(ingredients[0])])
+        tfidfs.append(datapoint_tfidf)
+    return ds.with_columns(pl.Series(name="tfidf", values=tfidfs))
---
+++ app/data_handling/NER_parquet.py
@@ -0,0 +1,111 @@
+import spacy
+import polars as pl
+import json
+from app.data_handling.preprocessing import nlp
+vocab = set()
+
+
+def process_ingredient(ingredient_doc: spacy.tokens.doc.Doc,
+                       include_variety: bool = False):
+    food_lemmas = []
+    prep_lemmas = [[]]
+    optional = False
+    if include_variety:
+        var_lemmas = [[]]
+    for t in ingredient_doc.ents:
+        if t.label_ == 'Food':
+            lemmas = [w.lemma_.lower() for w in nlp(str(t))]
+            food_lemmas.append(lemmas)
+            if len(prep_lemmas[-1]) > 0:
+                # Add a new list of preparation that corresponds to a
+                # new food item on the same line
+                prep_lemmas.append([])
+            if include_variety and len(var_lemmas[-1]) > 0:
+                var_lemmas.append([])
+        elif t.label_ == 'Preparation':
+            lemmas = [w.lemma_.lower() for w in nlp(str(t))]
+            prep_lemmas[-1] = prep_lemmas[-1] + lemmas
+        elif include_variety and t.label_ == 'Variety':
+            lemmas = [w.lemma_.lower() for w in nlp(str(t))]
+            var_lemmas[-1] = var_lemmas[-1] + lemmas
+        elif t.label_ == "Optional":
+            optional = True
+    ret_obj = {'food': food_lemmas,
+               'prep': prep_lemmas,
+               'optional': optional}
+    if include_variety:
+        ret_obj['var': var_lemmas]
+    return ret_obj
+
+
+def transform_ingredients_to_tokens(ingredients: list,
+                                    ner_model: spacy.lang.en.English,
+                                    create_vocab=False):
+    ner_lines = [ner_model(ingredient_line) for ingredient_line in ingredients]
+    foods = []
+    preparations = {}
+    optionals = []
+    for line in ner_lines:
+        processed_line_obj = process_ingredient(line)
+        if len(processed_line_obj['food']) == 0:  # No food found, continue
+            continue
+        # Only take first food item in a line
+        food = "_".join(processed_line_obj['food'][0])
+        foods.append(food)
+        # If there's only one food, all prep would belong to that food
+        if len(processed_line_obj['food']) == 1:
+            preparation_items = [item for sublist in processed_line_obj['prep']
+                                 for item in sublist]
+        # Otherwise, we assume only the first list of preparations
+        # corresponds to the first food
+        else:
+            preparation_items = processed_line_obj['prep'][0]
+        if len(preparation_items) > 0:
+            preparations[food] = preparation_items
+        optionals.append(processed_line_obj['optional'])
+        if create_vocab:
+            vocab.add(food)
+    datapoint_obj = {
+        'foods': foods,
+        'preps': preparations,
+        'optionals': optionals
+        }
+    return datapoint_obj
+
+
+# Assume recipeNLG dataset, where each ingredient line is a list item
+def transform_ds_to_tokens(ds: pl.dataframe.frame.DataFrame,
+                           ner_model: spacy.lang.en.English,
+                           create_vocab=False):
+    tokens = []
+    preps = []
+    optionals = []
+    for i, ingredients in enumerate(ds['ingredients']):
+        datapoint_obj = transform_ingredients_to_tokens(ingredients, ner_model,
+                                                        create_vocab)
+        tokens.append(datapoint_obj['foods'])
+        preps.append(json.dumps(datapoint_obj['preps']))
+        optionals.append(datapoint_obj['optionals'])
+    new_ds = ds.with_columns(
+        pl.Series(name='tokens', values=tokens),
+        pl.Series(name='preps', values=preps),
+        pl.Series(name='optionals', values=optionals)
+    )
+    return new_ds
+
+
+def construct_ingredient_query(pq_path: str, ingredients: list, preps: dict):
+    prep_keys = list(preps.keys())
+    base_sql = (f"SELECT *, json_extract(preps, {prep_keys}) AS prep_lists"
+                f" FROM '{pq_path}'"
+                " AS tbl"
+                f" WHERE list_has_all(tbl.tokens, {ingredients})"
+                )
+    prep_filter = ""
+    for i, (key, value) in enumerate(preps.items()):
+        prep_filter += (f" AND (prep_lists->>{i}) IS NOT NULL"
+                        f" AND list_has_all(from_json(prep_lists->>{i},"
+                        f" '[\"VARCHAR\"]'), {value})"
+                        )
+    sql = base_sql + prep_filter
+    return sql
