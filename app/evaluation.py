724463c983f62806d2e81af1ee428396281eb787
NaabZer <anton.gefvert@gmail.com>
2025-06-09 20:04:04 +0200

    Add precision@k evaluation

--- app/evaluation.py
+++ app/evaluation.py
@@ -3,6 +3,13 @@ import scipy
 from sklearn.metrics.pairwise import cosine_similarity
 
 
+def precision_at_k(ranked_ds: pl.dataframe.frame.DataFrame,
+                   correct_idx: int, k: int = 3):
+    # divide by one, since we only ever have one correct recipe in these tests
+    return ranked_ds.slice(0, k)\
+            .filter(pl.col("index") == correct_idx).shape[0] / 1
+
+
 def reciprocal_rank(ranked_ds: pl.dataframe.frame.DataFrame, correct_idx: int):
     filtered_ds = ranked_ds.with_row_index("id", offset=1)\
             .filter(pl.col("index") == correct_idx)
