97a9dd01cf95db9ea8c2da3ac837c8622fbaba7a
NaabZer <anton.gefvert@gmail.com>
2025-06-11 11:22:13 +0200

    Add exclude list, varieties and brands to NER processing

--- app/data_handling/NER_parquet.py
+++ app/data_handling/NER_parquet.py
@@ -5,6 +5,15 @@ from pydantic import BaseModel
 from app.data_handling.preprocessing import nlp
 
 
+exclude_list = [
+        'salt',
+        'pepper',
+        'black_pepper',
+        'water',
+        'ice'
+        ]
+
+
 class ParquetDefinition(BaseModel):
     name: str
     data: list
@@ -12,78 +21,179 @@ class ParquetDefinition(BaseModel):
 
 
 def process_ingredient(ingredient_doc: spacy.tokens.doc.Doc,
-                       include_variety: bool = False):
+                       include_variety: bool = False,
+                       include_brand: bool = False
+                       ):
     food_lemmas = []
-    prep_lemmas = [[]]
+    prep_lemmas = []
     optional = False
     if include_variety:
-        var_lemmas = [[]]
+        var_lemmas = []
+    if include_brand:
+        brand_lemmas = []
     for t in ingredient_doc.ents:
         if t.label_ == 'Food':
             lemmas = [w.lemma_.lower() for w in nlp(str(t))]
             food_lemmas.append(lemmas)
-            if len(prep_lemmas[-1]) > 0:
-                # Add a new list of preparation that corresponds to a
-                # new food item on the same line
-                prep_lemmas.append([])
-            if include_variety and len(var_lemmas[-1]) > 0:
+            # Add a new list of preparation that corresponds to a
+            # new food item on the same line
+            prep_lemmas.append([])
+            if include_variety:
                 var_lemmas.append([])
+            if include_brand:
+                brand_lemmas.append([])
         elif t.label_ == 'Preparation':
             lemmas = [w.lemma_.lower() for w in nlp(str(t))]
+            if len(prep_lemmas) == 0:
+                prep_lemmas.append([])
             prep_lemmas[-1] = prep_lemmas[-1] + lemmas
         elif include_variety and t.label_ == 'Variety':
             lemmas = [w.lemma_.lower() for w in nlp(str(t))]
+            if len(var_lemmas) == 0:
+                var_lemmas.append([])
             var_lemmas[-1] = var_lemmas[-1] + lemmas
+        elif include_brand and t.label_ == 'Brand':
+            lemmas = [w.lemma_.lower() for w in nlp(str(t))]
+            if len(brand_lemmas) == 0:
+                brand_lemmas.append([])
+            brand_lemmas[-1] = brand_lemmas[-1] + lemmas
         elif t.label_ == "Optional":
             optional = True
     ret_obj = {'food': food_lemmas,
                'prep': prep_lemmas,
                'optional': optional}
     if include_variety:
-        ret_obj['var': var_lemmas]
+        ret_obj['var'] = var_lemmas
+    if include_brand:
+        ret_obj['brand'] = brand_lemmas
     return ret_obj
 
 
-def transform_ingredients_to_tokens(ingredients: list,
-                                    ner_model: spacy.lang.en.English):
+def transform_ingredients_to_tokens(
+        ingredients: list[str] | str,
+        ner_model: spacy.lang.en.English,
+        include_variety: bool = False,
+        include_brand: bool = False,
+        use_alternate_food: bool = False,
+        remove_optional: bool = False,
+        exclude_list: list[str] | None = exclude_list
+        ):
     ner_lines = [ner_model(ingredient_line) for ingredient_line in ingredients]
     foods = []
     preparations = {}
     optionals = []
+    if include_variety:
+        varieties = {}
+    if include_brand:
+        brands = {}
+    if use_alternate_food:
+        alt_foods = []
     for line in ner_lines:
-        processed_line_obj = process_ingredient(line)
+        processed_line_obj = process_ingredient(
+                line,
+                include_variety=include_variety,
+                include_brand=include_brand
+                )
         if len(processed_line_obj['food']) == 0:  # No food found, continue
             continue
-        # Only take first food item in a line
+        if remove_optional and processed_line_obj['optional']:
+            continue
+
+        # If there's only one food, all preps etc would belong to that food
         food = "_".join(processed_line_obj['food'][0])
+        if food in exclude_list:
+            continue
         foods.append(food)
-        # If there's only one food, all prep would belong to that food
         if len(processed_line_obj['food']) == 1:
             preparation_items = [item for sublist in processed_line_obj['prep']
                                  for item in sublist]
-        # Otherwise, we assume only the first list of preparations
-        # corresponds to the first food
+            if include_variety:
+                variety_items = [item for sublist in processed_line_obj['var']
+                                 for item in sublist]
+            if include_brand:
+                brand_items = [item for sublist in processed_line_obj['brand']
+                               for item in sublist]
+        # Otherwise, we assume the prep list correspond to the ingredient list
         else:
             preparation_items = processed_line_obj['prep'][0]
+            if include_variety:
+                variety_items = processed_line_obj['var'][0]
+            if include_brand:
+                brand_items = processed_line_obj['brand'][0]
         if len(preparation_items) > 0:
-            preparations[food] = preparation_items
+            preparations[food] = preparations.get(food, []) + preparation_items
+        if include_variety and len(variety_items) > 0:
+            varieties[food] = varieties.get(food, []) + variety_items
+        if include_brand and len(brand_items) > 0:
+            brands[food] = brands.get(food, []) + brand_items
+
+        # Add the rest of the food in the same line as a separate datapoint
+        if use_alternate_food:
+            try:
+                for i, alt_food in enumerate(processed_line_obj['food'][1:]):
+                    food = "_".join(alt_food)
+                    alt_foods.append(food)
+                    preparation_items = processed_line_obj['prep'][i+1]
+                    if len(preparation_items) > 0:
+                        preparations[food] = \
+                                preparations.get(food, []) + preparation_items
+
+                    if include_variety:
+                        variety_items = processed_line_obj['var'][i+1]
+                        if len(variety_items) > 0:
+                            varieties[food] = \
+                                    varieties.get(food, []) + variety_items
+                    if include_brand:
+                        brand_items = processed_line_obj['brand'][i+1]
+                        if len(brand_items) > 0:
+                            brands[food] = brands.get(food, []) + brand_items
+            except Exception as e:
+                print(e)
+                print(f"alt_food: {alt_food}")
+                print(f"food: {processed_line_obj['food']}")
+                print(f"prep: {processed_line_obj['prep']}")
+                print(f"var: {processed_line_obj['var']}")
+                print(f"brand: {processed_line_obj['brand']}")
         optionals.append(processed_line_obj['optional'])
     datapoint_obj = {
         'foods': foods,
         'preps': preparations,
         'optionals': optionals
         }
+    if use_alternate_food:
+        datapoint_obj['alt_foods'] = alt_foods
+    if include_variety:
+        datapoint_obj['vars'] = varieties
+    if include_brand:
+        datapoint_obj['brands'] = brands
     return datapoint_obj
 
 
 # Assume recipeNLG dataset, where each ingredient line is a list item
 def transform_data_to_tokens(data: list[str],
-                             ner_model: spacy.lang.en.English):
+                             ner_model: spacy.lang.en.English,
+                             include_variety: bool = False,
+                             include_brand: bool = False,
+                             use_alternate_food: bool = False,
+                             remove_optional: bool = False,
+                             exclude_list: list[str] | None = exclude_list):
     tokens = []
     preps = []
     optionals = []
+    if include_variety:
+        varieties = []
+    if include_brand:
+        brands = []
+    if use_alternate_food:
+        alt_foods = []
     for ingredients in data:
-        datapoint_obj = transform_ingredients_to_tokens(ingredients, ner_model)
+        datapoint_obj = transform_ingredients_to_tokens(
+                ingredients, ner_model,
+                include_variety=include_variety,
+                include_brand=include_brand,
+                use_alternate_food=use_alternate_food,
+                remove_optional=remove_optional
+                )
         tokens.append(datapoint_obj['foods'])
         prep_dict = datapoint_obj['preps']
         prep = []
@@ -91,7 +201,28 @@ def transform_data_to_tokens(data: list[str],
             prep.append({'key': k, 'value': v})
         preps.append(prep)
         optionals.append(datapoint_obj['optionals'])
-    return tokens, preps, optionals,
+        if include_variety:
+            var_dict = datapoint_obj['vars']
+            var = []
+            for k, v in var_dict.items():
+                var.append({'key': k, 'value': v})
+            varieties.append(var)
+        if include_brand:
+            brand_dict = datapoint_obj['brands']
+            brand = []
+            for k, v in brand_dict.items():
+                brand.append({'key': k, 'value': v})
+            brands.append(brand)
+        if use_alternate_food:
+            alt_foods.append(datapoint_obj['alt_foods'])
+    ret_obj = [tokens, preps, optionals]
+    if include_variety:
+        ret_obj.append(varieties)
+    if include_brand:
+        ret_obj.append(brands)
+    if use_alternate_food:
+        ret_obj.append(alt_foods)
+    return ret_obj
 
 
 def create_parquet_file(parquet_path: str, df: pl.DataFrame,
